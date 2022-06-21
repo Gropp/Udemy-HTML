f9805621868e261717bfe9912eb45e12dafb7b28
Gropp <fgropp@gmail.com>
2022-06-20 23:46:37 -0300

    galeria site3

---
+++ SITE3/javascript/script.js
@@ -0,0 +1,23 @@
+// SINTAXE JQUERY $
+$(function() {
+    $('.botaoMenu').click(function(event){
+        event.stopPropagation();
+        if(!$(this).hasClass('menuAtivo')){
+            $(this).addClass('menuAtivo');
+            $('nav ul').animate({'left':'0px'},300);
+        }else {
+            $(this).removeClass('menuAtivo');
+            $('nav ul').animate({'left':'-100%'},300);
+        }
+    });
+
+    $('body').on('click',function(){
+        $('.botaoMenu').removeClass('menuAtivo');
+        $('nav ul').animate({'left':'-100%'},300);
+    });
+
+    Fancybox.bind('[data-fancybox="gallery"]', {
+        infinite: false,
+        zoom: true,
+    });
+});
